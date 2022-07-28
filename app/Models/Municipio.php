a6a9de83a0743ae191b811196caffa028f36405f
Franco Glinyer <gfrancojf@gmail.com>
2022-07-28 19:14:06 -0400

    se agrego  la auditoria y softdelets

--- app/Models/Municipio.php
+++ app/Models/Municipio.php
@@ -4,16 +4,19 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Municipio extends Model 
+class Municipio extends Model  implements Auditable
 
 
 {
     use HasFactory;
-
+    use \OwenIt\Auditing\Auditable;
 
 	protected $table = 'municipios';
 	public $timestamps = false;
+    protected $dates = ['deleted_at'];
+
 
     public function estado()
     {
