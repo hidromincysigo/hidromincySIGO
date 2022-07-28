656641f8a62672da86045990121190006e961f45
Franco Glinyer <gfrancojf@gmail.com>
2022-07-28 15:40:49 -0400

    se corrigue error

---
+++ database/migrations/2022_07_28_153213_create_parroquias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parroquias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('municipio_id');     
+	        $table->string('parroquia')->comment('nombre de la parroquia');
+	        $table->foreign('municipio_id')->references('id')->on('municipios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parroquias');
+    }
+};
