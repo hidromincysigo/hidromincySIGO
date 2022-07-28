f0c5d1c38369acac5358badf5445707f253978f0
Franco Glinyer <gfrancojf@gmail.com>
2022-07-28 15:36:17 -0400

    se corrigue error en la migracion

---
+++ database/migrations/2022_07_28_153041_create_ciudades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ciudades', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('estado_id'); 
+	        $table->string('ciudad')->comment('nombre del ciudad');
+	        $table->tinyInteger('capital');
+	        $table->foreign('estado_id')->references('id')->on('estados');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ciudades');
+    }
+};
