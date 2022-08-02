2aa525ccb34d8960ac878a3c0cba35c067325e9e
gfranco <gfrancojf@gmail.com>
2022-08-02 12:45:04 -0400

    roles en acueducto

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -50,7 +50,6 @@
                                     </td>
 
                                     <td>                                  
-                                      <a class="btn btn-info" href="{{ route('usuarios.edit',$usuario->id) }}">Editar</a>
 
                                       {!! Form::open(['method' => 'DELETE','route' => ['usuarios.destroy', $usuario->id],'style'=>'display:inline']) !!}
                                           {!! Form::submit('Borrar', ['class' => 'btn btn-danger']) !!}
